f77474f76cbb8a6d42af9073971a3fe5619890f9
Matar-Ben-Shitrit <60197023+Matar-Ben-Shitrit@users.noreply.github.com>
2023-02-23 13:24:19 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,10 @@
 public class main
 {
-    public main()
+    public static void main(String[] args)
     {
         int[] s1Grades= {90,89,95,78,99};
         student s1=new student("Matar","BSH","1020304050",s1Grades);
         System.out.println(s1.gradeAVG());
     }
+    
 }
