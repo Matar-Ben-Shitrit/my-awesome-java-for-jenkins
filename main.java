88a296ff411352050edd66e4b00abf72532014d3
Matar-Ben-Shitrit <60197023+Matar-Ben-Shitrit@users.noreply.github.com>
2023-02-23 11:35:27 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main
+{
+    public main()
+    {
+        int[] s1Grades= {90,89,95,78,99};
+        student s1=new student("Matar","BSH","1020304050",s1Grades);
+        System.out.println(s1.gradeAVG());
+    }
+}
